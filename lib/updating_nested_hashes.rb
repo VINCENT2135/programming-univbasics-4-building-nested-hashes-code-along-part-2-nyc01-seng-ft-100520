a1ad8a4070e66d2c793b93ced24c2f7b78cf9d25
Vincent Ivos <vincentivos1@gmail.com>
2020-09-24 01:06:53 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -54,7 +54,7 @@ epic_tragedy = {
   }
 }
 
-
+def add_characters
 epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
 epic_tragedy[:additional_characters] << { name: "Apothecary" }
 
